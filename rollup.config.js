18310a3f6c8a09e452ee12f09726a06da8ccd2aa
mhmd-22 <mohammedqurafi@gmail.com>
2021-05-04 01:00:55 +0300

    Fix missing "!" on dispatch custom event

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import { terser } from "rollup-plugin-terser";
 
-const files = ["drag", "pinch", "index", "longpress"];
+const files = ["drag", "pinch", "index"];
 
 export default files.map((file) => ({
     input: `lib/${file}.js`,
@@ -10,6 +10,14 @@ export default files.map((file) => ({
         name: "EZG",
         extend: file !== "index",
         sourcemap: true,
-        plugins: [terser()],
+        // plugins: [terser()],
+    },
+    output: {
+        file: `demos/dist/${file}.min.js`,
+        format: "umd",
+        name: "EZG",
+        extend: file !== "index",
+        sourcemap: true,
+        // plugins: [terser()],
     },
 }));
